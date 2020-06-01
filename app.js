7b585edf6e5c9a0faf3c29aa79eff1cb600cc5e6
Poojitha <poojithabalanagu@gmail.com>
2020-06-01 15:02:03 +0530

    First web file.

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 (function () {
+
 'use strict';
+angular.module('myFirstAssig',[])
+.controller('MsgController',MsgController);
 
-angular.module('DIApp', [])
-.controller('DIController', DIController);
+MsgController.$inject = ['$scope'];
+function MsgController($scope){
+	$scope.result = "";
+	$scope.input = "";
+	$scope.colorResult = "black";
+	$scope.borderColor = "Black";
+	$scope.checkLunch = function(){
+		if ($scope.input == "") {
+				$scope.result = "PLEASE ENTER THE DATA FIRST";
+				$scope.colorResult = "Red";
+				$scope.borderColor = "Red";
+		}
+		else
+		{
+			var length = (($scope.input).split(",")).length;
+			if(length > 0 && length < 4)
+			{
+				$scope.result = "Enjoy!";
+				$scope.colorResult = "#4dff4d";
+				$scope.borderColor = "#4dff4d";
 
-function DIController ($scope) {
-  $scope.name = "Poojitha";
+			}
+			else if(length > 3 && length >0)
+			{
+				$scope.result = "Too Much";
+				$scope.colorResult = "#4dff4d";
+				$scope.borderColor = "#4dff4d";
+			}
 
+			}
+
+	};
 }
 
 })();
